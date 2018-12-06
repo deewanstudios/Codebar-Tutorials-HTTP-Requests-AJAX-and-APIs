f60cac5980d9cc4a4553f997f1f5d9127450e5df
Adedayo Adedapo <daniel@deewanstudios.com>
2018-12-06 11:45:29 +0000

    Commiting my HTTP Ajax Request part one, from the Codebar tutorials
    	=> Added document ready callback funtion.
    	=> Subscribed to the on keypress event on the username id
    	element
    	=> Checked if the value of the key pressed is 13 (Which is the
    	Enter Key)
    	=> Captured the username input into the input variable
    	=> Captured the value of the input into the username variable
    	=> Called the showUser function and passed the getGitHubInfo
    	function to it as a parameter, with the username name as
    	parameter for the getGitHubInfo function.

---
+++ script.js
@@ -0,0 +1,40 @@
+// github user finder example
+$(document).ready(function () {
+    $(document).on('keypress', '#username', function (event) {
+        if (event.which === 13) {
+            // console.log("Enter Key Pressed");
+            var input = $(this);
+            var username = input.val();
+            console.log('username was: ' + username);
+            showUser(getGitHubInfo(username));
+        }
+    });
+
+});
+
+function getGitHubInfo(username) {
+    var url = 'https://api.github.com/users/' + username;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('GET', url, false);
+    xmlhttp.send();
+    return xmlhttp;
+    // console.log(data);
+    // var data = xmlhttp.responseText;
+}
+
+function showUser(xmlhttp) {
+    if (xmlhttp.status === 200) {
+        //  Show the user details
+        // console.log(xmlhttp.status);
+        var dataString = xmlhttp.responseText;
+        var user = JSON.parse(dataString);
+        // console.log(user);
+        $('#profile h2').html(user.login + ' is GitHub user #' + user.id);
+        $('#profile .information').html('<a href="' + user.html_url + '" class="profile">' + user.name + '</a>');
+        $('#profile .avatar').html('<img src="' + user.avatar_url + '">');
+
+    } else {
+        // Show an error;
+
+    }
+}
